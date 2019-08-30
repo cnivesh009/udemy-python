a30d5a08ff485dc3efb16255c31e028e71f3d028
cnivesh009 <cnivesh@hotmail.com>
2019-08-30 21:28:18 +0530

    update 4 String indexing.py

--- 4 String indexing.py
+++ 4 String indexing.py
@@ -1,6 +1,8 @@
-# Date: 15.07.2019
+# Document  : 4 String indexing.py
+# Created on : 15-07-2019  09:31:27 PM
+# Author       : Nivesh-GC
+
 # Udemy: Complete Python BootCamp
-# Last Edited: 16.07.2019
 
 # Displays r from the string 'Hello world'
 
