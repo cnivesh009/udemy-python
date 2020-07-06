06a78900d3fb58046bfc6152d00e1bf64cd49c1a
Nivesh Chandola <cnivesh@hotmail.com>
2020-07-06 09:49:15 +0530

    Update 1 HelloWorld.py

--- 1 HelloWorld.py
+++ 1 HelloWorld.py
@@ -1,14 +1,14 @@
 # Document  : 1 HelloWorld.py
-# Created on : 13-07-2019, 06:05:07 PM
-# Author       : Nivesh-GC
+# Created on: 13-07-2019, 06:05:07 PM
+# Author    : Nivesh-GC
 
 # Udemy: Complete Python BootCamp
 # https://www.youtube.com/watch?v=DjEuROpsvp4
-# Setting up a Python Development Environment in Atom
+# Setting up a Python Development Environment in Atom 
 
 print('Hello World of Atom')
 a = 50
-b = 50
+b = 50    
 c = a + b
 d = type(c)
 print(d)
