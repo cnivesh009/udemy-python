3cdc9954db72b39893c78884fad47e269238e4ca
cnivesh009 <cnivesh@hotmail.com>
2020-07-06 13:46:14 +0530

    modified to repo

--- 1 HelloWorld.py
+++ 1 HelloWorld.py
@@ -4,11 +4,11 @@
 
 # Udemy: Complete Python BootCamp
 # https://www.youtube.com/watch?v=DjEuROpsvp4
-# Setting up a Python Development Environment in Atom 
+# Setting up a Python Development Environment in Atom
 
 print('Hello World of Atom')
 a = 50
-b = 50    
+b = 50
 c = a + b
 d = type(c)
 print(d)
